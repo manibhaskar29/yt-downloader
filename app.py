2c0427ba943db5b7a6f3f1edbb82e71ef4ce50fc
Kenguva Mani bhaskar <kenguva.manibhaskar@gmail.com>
2025-08-31 11:37:57 +0530

    Update app.py added cors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import yt_dlp
+from flask_cors import CORS  # <-- add this
 
 app = Flask(__name__)
+CORS(app)  # <-- enable CORS for all routes
 
 # --- Helper Function ---
 def download_youtube(url, is_playlist=False):
